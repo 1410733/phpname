2f7ca6c9a97bee5760258153cc45f6c821da7b20
Santuraki <1410733@rgu.ac.uk>
2016-02-24 15:06:10 +0000

    0

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+`<p>
+    <?php
+    echo "Hello World";
+    ?>
+</p>
+</body>
+</html>
+/**
+* Created by PhpStorm.
+* User: 1410733
+* Date: 24/02/2016
+* Time: 14:36
+*/
