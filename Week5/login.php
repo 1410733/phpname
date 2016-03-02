97b64269f5100a9f50249232f983704561dda197
Santuraki <1410733@rgu.ac.uk>
2016-03-02 16:44:42 +0000

    kk

--- Week5/login.php
+++ Week5/login.php
@@ -1,22 +1,21 @@
-
+<?php
 /**
  * Created by PhpStorm.
  * User: 1410733
  * Date: 02/03/2016
  * Time: 15:59
  */
-<?php
+
 include ("Mysql.php");
 if (empty ($_POST["USERNAME"]) || empty ($_POST["password"])) {
     echo "Both fields are required.";
 }  else {
     $username = $_POST ['username'];
     $password = $POST ['password'];
-    $sql = "SELECT uid FROM users WHERE username='$username' and password=$'password'";
+    $sql = "SELECT uid FROM users WHERE username='$username' and password='$password'";
     $result = mysql_query($db, $sql);}
     if (mysqli_num_rows($result) == 1) {
         header("location: home.php"); // Redirecting to another page
     } else {
         echo "Incorrect username or password.";
 }
-?>
