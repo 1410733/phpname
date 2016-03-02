5424824707b43d5cba4d10e4881beed312fe0727
Santuraki <1410733@rgu.ac.uk>
2016-03-02 16:57:26 +0000

    kk

--- Week5/Mysql.php
+++ Week5/Mysql.php
@@ -1,11 +1,11 @@
-
+<?php
 /**
  * Created by PhpStorm.
  * User: 1410733
  * Date: 02/03/2016
  * Time: 15:39
  */
-<?php
+
 $DB_SERVER='ap-cdbr-azure-east-c.cloudapp.net';
 $DB_USERNAME='b76bb32cfc0469';
 $DB_PASSWORD='378ffb7b';
