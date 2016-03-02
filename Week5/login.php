d3a0c2b80bf092d2cbc2bb4d3a1fc13268746660
Santuraki <1410733@rgu.ac.uk>
2016-03-02 16:49:33 +0000

    kk

--- Week5/login.php
+++ Week5/login.php
@@ -6,16 +6,17 @@
  * Time: 15:59
  */
 
-include ("Mysql.php");
+include("Mysql.php");
 if (empty ($_POST["USERNAME"]) || empty ($_POST["password"])) {
     echo "Both fields are required.";
-}  else {
+} else {
     $username = $_POST ['username'];
     $password = $POST ['password'];
     $sql = "SELECT uid FROM users WHERE username='$username' and password='$password'";
-    $result = mysql_query($db, $sql);}
-    if (mysqli_num_rows($result) == 1) {
-        header("location: home.php"); // Redirecting to another page
-    } else {
-        echo "Incorrect username or password.";
+    $result = mysql_query($db, $sql);
+}
+if (mysqli_num_rows($result) == 1) {
+    header("location: home.php"); // Redirecting to another page
+} else {
+    echo "Incorrect username or password.";
 }
