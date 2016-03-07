00a0f5760389ee8b66733b5976d7cd5eb5ab05b7
Lesson <1410733@rgu.ac.uk>
2016-03-07 20:35:41 +0000

    new from laptop

--- Trials/bugs.php
+++ Trials/bugs.php
@@ -9,6 +9,10 @@
     echo "I am dimocee";
     echo "Hello, Binta";
     echo "Hello," . " " . "Binta " . "!";
+    echo 5 * 40;
+    $myname = "Modee Santuraki";
+    $myage = 231;
+    echo "My name is" . $myname . "and I am" . $myage . "Years old";
 
 ?>
 </p>
