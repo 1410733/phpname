6fb1b83da470bdbd5d3c4ce7651acfd05914e629
Lesson <1410733@rgu.ac.uk>
2016-03-09 14:03:00 +0000

    new from laptop

---
+++ Week5/homework/homeconnection.php
@@ -0,0 +1,15 @@
+/**
+* Created by PhpStorm.
+* User: Lesson
+* Date: 09/03/2016
+* Time: 01:03 PM
+*/
+
+
+<?php
+define('DB_SERVER', 'http://santuraki.azurewebsites.net');
+define('DB_USERNAME', 'b76bb32cfc0469');
+define('DB_PASSWORD', '378ffb7b');
+define('DB_DATABASE', 'Santuraki 1410733');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
