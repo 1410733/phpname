1c013121c1f90ed44514f002aa0a35a26e3bd8eb
Lesson <1410733@rgu.ac.uk>
2016-03-12 01:54:11 +0000

    new from laptop

--- Coursework/login.php
+++ Coursework/login.php
@@ -12,9 +12,9 @@ if (empty ($_POST["username"]) || empty ($_POST["password"]))
     echo "Both fields are required.";
 } else
 {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $sql = "SELECT uid FROM users WHERE username='$username' and password='$password'";
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
     $result = mysqli_query($db,$sql);
 
 if (mysqli_num_rows($result) == 1) {
