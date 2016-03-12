88beba29d5fa817b1625866415fb7eb94fb34cb7
Lesson <1410733@rgu.ac.uk>
2016-03-12 01:41:20 +0000

    new from laptop

--- Coursework/login.php
+++ Coursework/login.php
@@ -7,17 +7,18 @@
  */
 <?php
 include("connection.php");
-if (empty ($_POST["username"]) || empty ($_POST["password"])) {
+if (empty ($_POST["username"]) || empty ($_POST["password"]))
+{
     echo "Both fields are required.";
-} else {
-    $username = $_POST ['username'];
-    $password = $_POST ['password'];
+} else
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
     $sql = "SELECT username FROM homepractice WHERE username='$username' and password='$password'";
-    $result = mysqli_query($db, $sql);
+    $result = mysqli_query($db,$sql);
 }
 if (mysqli_num_rows($result) == 1) {
     header("location: home.php"); // Redirecting to another page
 } else {
     echo "Incorrect username or password.";
-}
-?
+} ?>
