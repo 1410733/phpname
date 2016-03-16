1490d4fc4abc1d372aed6dba1413ad8a611cb5da
Lesson <1410733@rgu.ac.uk>
2016-03-16 15:12:55 +0000

    new from laptop

--- registration/connection.php
+++ registration/connection.php
@@ -1,14 +1,8 @@
-
-/**
- * Created by PhpStorm.
- * User: Lesson
- * Date: 12/03/2016
- * Time: 12:47 AM
- */
 <?php
 define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'b76bb32cfc0469');
 define('DB_PASSWORD', '378ffb7b');
-define('DB_DATABASE', 'Santuraki1410733');
+define('DB_DATABASE', 'Santuraki 1410733');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
 ?>
