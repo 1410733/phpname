b80b8403b75d4d6cbc3b3d10682ce9f5b07f6ed5
Lesson <1410733@rgu.ac.uk>
2016-03-16 15:46:05 +0000

    new from laptop

--- registration/connection.php
+++ registration/connection.php
@@ -5,4 +5,10 @@ define('DB_PASSWORD', '15106d56');
 define('DB_DATABASE', 'Santuraki1410733');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
 ?>
