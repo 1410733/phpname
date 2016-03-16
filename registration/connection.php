8cdf0c3464a92149587eca8e6f50e67e8992bbb3
Lesson <1410733@rgu.ac.uk>
2016-03-16 22:15:37 +0000

    new from laptop

--- registration/connection.php
+++ registration/connection.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
-define('DB_USERNAME', 'b99256e599d9fc');
-define('DB_PASSWORD', '64c23a04');
-define('DB_DATABASE', 'rgutest');
+define('DB_USERNAME', 'be6a3e71586ff2');
+define('DB_PASSWORD', '15106d56');
+define('DB_DATABASE', 'santuraki1410733');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
