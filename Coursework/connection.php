c4cb02ff9caad9efb0af56681ea0f34a3abfff71
Lesson <1410733@rgu.ac.uk>
2016-03-16 15:51:17 +0000

    new from laptop

--- Coursework/connection.php
+++ Coursework/connection.php
@@ -6,9 +6,15 @@
  * Time: 12:47 AM
  */
 <?php
-define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-define('DB_USERNAME', 'b76bb32cfc0469');
-define('DB_PASSWORD', '378ffb7b');
+define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
+define('DB_USERNAME', 'be6a3e71586ff2');
+define('DB_PASSWORD', '15106d56');
 define('DB_DATABASE', 'Santuraki1410733');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
 ?>
