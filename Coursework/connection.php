fd4925e9251f4fa3228d03532d0c010c953ea9da
Lesson <1410733@rgu.ac.uk>
2016-03-18 17:29:41 +0000

    new from laptop

--- Coursework/connection.php
+++ Coursework/connection.php
@@ -1,11 +1,11 @@
-
+<?php
 /**
  * Created by PhpStorm.
  * User: Lesson
  * Date: 12/03/2016
  * Time: 12:47 AM
  */
-<?php
+
 define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
 define('DB_USERNAME', 'be6a3e71586ff2');
 define('DB_PASSWORD', '15106d56');
