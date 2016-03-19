25ecdad4b7d5d313da8c334b94f7bb698d4410f6
Lesson <1410733@rgu.ac.uk>
2016-03-19 18:38:38 +0000

    new from laptop

--- Coursework/login.php
+++ Coursework/login.php
@@ -7,6 +7,7 @@
  */
 
 include("connection.php");
+    SESSION_START ();
 if (empty ($_POST["username"]) || empty ($_POST["password"]))
 {
     echo "Both fields are required.";
@@ -26,6 +27,8 @@ if (empty ($_POST["username"]) || empty ($_POST["password"]))
 
 if (mysqli_num_rows($result) == 1 ) {
     header("location: home.php"); // Redirecting to another page
+        $_SESSION['username']=$username;
+    echo "<br />How are you today".$_SESSION['username'];
 } else {
     echo "Incorrect username or password."; }
 } ?>
