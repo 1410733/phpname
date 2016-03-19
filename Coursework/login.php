a505052372f7cbd6fc4f3f666362320e2feab745
Lesson <1410733@rgu.ac.uk>
2016-03-19 19:46:59 +0000

    new from laptop

--- Coursework/login.php
+++ Coursework/login.php
@@ -17,7 +17,7 @@ if (empty ($_POST["username"]) || empty ($_POST["password"]))
     $password=$_POST['password'];
     $password = mysqli_real_escape_string($db, $password);
     $password = md5($password);
-    $_SESSION['usermail'] = $username;
+    $_SESSION['username'] = $username;
 
 
     $sql="SELECT * FROM users WHERE username='$username' and password='$password'";
