12631a7438be6fe2e2bb1e42c8fd5091f262194a
Lesson <1410733@rgu.ac.uk>
2016-03-19 17:12:56 +0000

    new from laptop

--- Coursework/home.php
+++ Coursework/home.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="utf-8">
     <title>PHP Login Form without Session</title>
+
 </head>
 
 <body>
+<a href="Bugform.php">Create Bug</a>
 <h1>Now you finally broke through</h1>
 
 
