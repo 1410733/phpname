ad71a67f82b76fbe8e37a0c81a5d465148f894f6
Lesson <1410733@rgu.ac.uk>
2016-03-20 21:53:29 +0000

    new from laptop

--- Coursework/Bug.php
+++ Coursework/Bug.php
@@ -27,7 +27,7 @@ if(isset($_POST["submit"])) {
     //$userID = $sql['userID'];
    // echo $userID;
   //  echo "two";
-    $sql="Insert into bugs (title, descr, postDate, userID ) VALUES ('$Bugtitle', '$BugDesc', now(), '$userID')";
+    $sql="Insert into bugs (title, descr, postDate, userID ) VALUES ('$Bugtitle', '$BugDesc', now(), '$userID')" or die(mysqli_error($db));
     $result=mysqli_query($db,$sql);
     echo "two";
     if($result)
