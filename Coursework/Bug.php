8ecd9399ba505a89cff2d3eeee110462809abb93
Lesson <1410733@rgu.ac.uk>
2016-03-20 21:19:27 +0000

    new from laptop

--- Coursework/Bug.php
+++ Coursework/Bug.php
@@ -5,6 +5,9 @@
  * Date: 18/03/2016
  * Time: 08:35 PM
  */
+SESSION_START();
+
+
 
 include ("connection.php");
 $msg = "";
@@ -13,17 +16,17 @@ if(isset($_POST["submit"])) {
     $Bugtitle = $_POST["Bugtitle"];
     $BugDesc = $_POST["BugDesc"];
     $Attachment = $_POST["Attachment"];
-    $PresentUser = $_POST {'id'};
+    $PresentUser = $_SESSION['username'] ;
 
     $Bugtitle= mysqli_real_escape_string($db, $Bugtitle);
     $BugDesc = mysqli_real_escape_string($db, $BugDesc);
     $Attachment = mysqli_real_escape_string($db, $Attachment);
 
-    $sql=mysqli_fetch_array(mysqli_query($db, "select * from users where email= $PresentUser"));
+    $sql=mysqli_fetch_array(mysqli_query($db, "select userID from users where username= $PresentUser"));
     $userID = $sql['userID'];
     echo $userID;
     echo "two";
-    $sql="Insert into bugs (title, descr, postDate, userID ) VALUES ('$Bugtitle', '$BugDesc', now(), 'UserID'";
+    $sql="Insert into bugs (title, descr, postDate, userID ) VALUES ('$Bugtitle', '$BugDesc', now(), '$userID')";
     $result=mysqli_query($db,$sql);
     if($result)
     {
