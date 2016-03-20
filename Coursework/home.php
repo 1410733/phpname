5ca82fa8d7699d604874d2701aa66426fdefede3
Lesson <1410733@rgu.ac.uk>
2016-03-20 18:06:16 +0000

    new from laptop

--- Coursework/home.php
+++ Coursework/home.php
@@ -8,7 +8,6 @@ $username= $_SESSION['username'] ;
 
 <!doctype html>
 
-
 <html>
 <head>
     <meta charset="utf-8">
@@ -18,7 +17,31 @@ $username= $_SESSION['username'] ;
 
 <body>
 <a href="Bugform.php">Create Bug</a>
-<?php echo $_SESSION["username"] . " ";?><h1>you have finally broke through... </h1>
+<?php echo $_SESSION["username"] . " ";?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<body>
+<div id="wrapper">
+    <h1>Tab Login & Sign Up Forms</h1>
+    <div id="container">
+        <section class="tabs">
+            <input id="tab-1" class="tab-selector-1" type="radio" checked="checked" name="radio-set">
+            <span class="tab-label-1" for="tab-1">Signup</span>
+            <input id="tab-2" class="tab-selector-2" type="radio" name="radio-set">
+            <span class="tab-label-2" for="tab-2">Login</span>
+            <input id="tab-3" class="tab-selector-3" type="radio" name="radio-set">
+            <span class="tab-label-3" for="tab-3">Forget Password</span>
+            <div class="clear-shadow"></div>
+            <div id="content">
+        </section>
+    </div>
+</div>
+<iframe id="SearchQuickKnow" style="width: 1px; height: 1px; display: none;" t="Browser">
+    <div id="gsdfcdiv">
+</body>
+</html>
 
 
 </body>
