dd97c3ca159311f30e99770f147a646c0a65ece0
Lesson <1410733@rgu.ac.uk>
2016-03-22 21:43:56 +0000

    new from laptop

--- Coursework/Bug.php
+++ Coursework/Bug.php
@@ -10,7 +10,25 @@ SESSION_START();
 
 include ("connection.php");
 $msg = "";
-if(isset($_POST["submit"])) {
+
+    if(isset($_POST["submit"])) {
+
+        if (getimagesize($_FILES['image']['tmp_name'])== FALSE)
+        {
+            echo "Please select an image.";
+        }
+        else {
+
+            $file_name = $_FILES['image']['name'];
+            $dir = $_FILES['image']['tmp_name'];
+            $location = "uploads/";
+            $fp = fopen($dir, 'r');
+            $content = fread($fp, filesize($dir));
+            $content = addslashes($content);
+            fclose($fp);
+            move_uploaded_file($dir, $location.$file_name);
+        }
+
 
     $Bugtitle = $_POST["Bugtitle"];
     $BugDesc = $_POST["BugDesc"];
@@ -38,7 +56,22 @@ if(isset($_POST["submit"])) {
     $query = mysqli_query($db, "INSERT INTO bugs (title, descr, postDate, userID) VALUES ('$Bugtitle', '$BugDesc', now(), '$xid')")
     or die(mysqli_error($db));
 
+        $sql1 = mysqli_query($db, "select * from bugs where title = '$Bugtitle'");
 
+while ($runsql = mysqli_fetch_array($sql1)) {
+    $newbugid = $runsql ['bugID'];
+}
+
+
+        $query1 = mysqli_query($db, "insert into attachments (URL, userID, bugID) VALUES ('$content', '$xid', '$newbugid')");
+        //$result = mysqli_query($query1);
+        if($query1) {
+            echo "<br/>Image Uploaded.";
+        }
+        else
+        {
+            echo "<br/>Image Not Uploaded.";
+        }
 
     if($query)
     {
@@ -49,37 +82,9 @@ if(isset($_POST["submit"])) {
     {
         echo "submission error";
     }
+
+    }
 }
+
 ?>
 
-<?php
-
-//File properties
-if(!isset($_POST['submit'])) {
-    if(getimagesize($_FILES['Attachment']['tmp_name']))== FALSE)
-
-    {
-        echo "Kindly choose an image";
-}
-    else
-    {
-        $image = addslashes($_FILES['image']['tmp_name']);
-        $name= addslashes($_FILES['image']['name']);
-        $image=file_get_contents($image);
-        $image=base64_decode($image);
-        $saveimages($image);
-    }
-
-}
-    function save image($name,$image)
-{
-    $qry=mysqli_query($db,"Insert INTO attachment (URL, userID, BugID) VALUES ('$image', '$xid', NULL )")
-    $result=mysqli_query($qry,$db);
-if($result)
-{
-    echo "<br/>Image uploaded successfully";
-} else
-{
-    echo "you got a problem";
-}
-}
