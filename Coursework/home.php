46e11a12064987d5d614f6a94463703536202d44
Lesson <1410733@rgu.ac.uk>
2016-03-23 00:49:02 +0000

    new from laptop

--- Coursework/home.php
+++ Coursework/home.php
@@ -16,7 +16,7 @@ $username= $_SESSION['username'] ;
 
 </head>
 <body>
-<p> Hello <?php echo $_SESSION["username"] . " ";?> </p>
+<p> Hello <em><?php echo $_SESSION["username"] . " ";?> </p></em>
 <br>
 <a href="Bugform.php">Create Bug</a>
 <a href="logout.php">Signout</a>
