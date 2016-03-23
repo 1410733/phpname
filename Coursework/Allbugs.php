d641d115913a9e299bced3b2af38239cd0825588
Lesson <1410733@rgu.ac.uk>
2016-03-23 16:03:28 +0000

    new from laptop

--- Coursework/Allbugs.php
+++ Coursework/Allbugs.php
@@ -1,11 +1,4 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Lesson
- * Date: 23/03/2016
- * Time: 03:43 PM
- */
-?>
+
 
 <?php
 //Establish connection with DB
@@ -19,10 +12,12 @@ $sql = "select * from bugs";
 $result = mysqli_query($db,$sql);
 
 //we scan each row
-while($row = mysqli_fetch_assoc($result)){
+while($row = mysqli_fetch_assoc($result)) {
     //get bug title and id
     $bugtitle = $row['title'];
     $bugID = $row['bugID'];
 
     //write link to page
-    echo "<a href='Allbugs2.php?id=".$bugID."'>".$bugtitle."</a></br>";
+    echo "<a href='Allbugs2.php?id=" . $bugID . "'>" . $bugtitle . "</a></br>";
+}
+?>
