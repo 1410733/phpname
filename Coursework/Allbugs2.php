232264bd232aca7afda0e789c89c573a9d30fb49
Lesson <1410733@rgu.ac.uk>
2016-03-24 16:27:43 +0000

    new from laptop

--- Coursework/Allbugs2.php
+++ Coursework/Allbugs2.php
@@ -34,18 +34,18 @@ echo "<p>".$bugdesc."</p>";
 </head>
 <body>
 <form>
-
+    <fieldset>
         <legend>Kindly leave a comment...</legend>
         <label for="name">Name:</label>
         <input type="text" name="name" value="" />
         <br>
-        <label for="email">Email:</label>
-        <input type="text" name="email" value="" />
         <br>
         <label for="comments">Comment:</label>
         <textarea name="comments" cols="45" rows="5"></textarea>
         <br>
+        <br>
         <input type="submit" value="Submit" />
+    </fieldset>
 </form>
 </body>
 </html>
