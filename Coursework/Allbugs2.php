b9526c4f009ad76864d9bee17490cbb11d1f8a0d
Lesson <1410733@rgu.ac.uk>
2016-03-25 20:52:55 +0000

    new from laptop

--- Coursework/Allbugs2.php
+++ Coursework/Allbugs2.php
@@ -66,8 +66,12 @@ $query2 = mysqli_query($db, "SELECT * FROM users WHERE username = '$currentUser'
 while ($rows = mysqli_fetch_array($query2)) {
     $xid = $rows['userID'];
 }
+$currentBugID = $_GET["id"];
 
-
+$query3 = mysqli_query($db, "SELECT * FROM bugs WHERE username = '$currentBugID'") or die (mysqli_error($db));
+while ($rows2 = mysqli_fetch_array($query3)) {
+    $xbugid = $rows2['bugID'];
+}
 
 //Select everything from our bugs table where ID is right
     $sql="select * from comments WHERE bugID=".$_GET["id"];
@@ -103,7 +107,7 @@ if(isset($_POST['submit'])){//to run PHP script on submit
     //echo $uid;
     // echo $comment;
 
-    $qry="INSERT  INTO comments (bugID, userID, descr, postDate) VALUES ('$intid','$xid','$comment', now())";
+    $qry="INSERT  INTO comments (bugID, userID, descr, postDate) VALUES ('$xbugid','$xid','$comment', now())";
 
     if(mysqli_query($db, $qry)){
         echo "Records added successfully.";
