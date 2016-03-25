171538a5bb8009e654a74e40a72ed316651c96d2
Lesson <1410733@rgu.ac.uk>
2016-03-25 15:30:34 +0000

    new from laptop

--- Coursework/login.php
+++ Coursework/login.php
@@ -8,7 +8,6 @@
 SESSION_START();
 $username = $_POST["username"];
 
-
 include("connection.php");
 
 if (empty ($_POST["username"]) || empty ($_POST["password"]))
@@ -25,19 +24,12 @@ if (empty ($_POST["username"]) || empty ($_POST["password"]))
 
     $sql="SELECT * FROM users WHERE username='$username' and password='$password'";
     $result = mysqli_query($db,$sql);
-    $row = mysqli_fetch_array($result);
-    $userID = $row['userID'];//get user id
-    $_SESSION=$userID;
+    $results = mysqli_fetch_array($result);
     echo $results['password'];
 
 if (mysqli_num_rows($result) == 1 ) {
     header("location: home.php"); // Redirecting to another page
     //echo "<br />How are you today".$_SESSION['username'];
-
-    //get the user id as a session variable
-
-    //$userid = $_SESSION["userID"];
-
 } else {
     echo "Incorrect username or password."; }
 } ?>
