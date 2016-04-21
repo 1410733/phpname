d05f7eed5e094e6d3fa164f3f65551f5b7a759b0
Lesson <1410733@rgu.ac.uk>
2016-04-21 21:45:44 +0100

    kk

--- photoapp/login.php
+++ photoapp/login.php
@@ -13,14 +13,14 @@
 			// Define $username and $password
 			$username=$_POST['username'];
 			$password=$_POST['password'];
-			$password = mysqli_real_escape_string($db, $password);
-			$password = md5($password);
+			//$password = mysqli_real_escape_string($db, $password);
+			//$password = md5($password);
 
 
 			
 			//Check username and password from database
 			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
-			$result=mysqli_query($db,$sql) or die('<pre> . Error!! . </pre> ');
+			$result=mysqli_query($db,$sql) or die ('<pre> . Error!! . </pre> ');
 			$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
 			
 			//If username and password exist in our database then create a session.
