3624583ca52468f57815b8a9daf018b317522fdc
Lesson <1410733@rgu.ac.uk>
2016-04-30 14:30:41 +0100

    procedure

--- photoapp/check.php
+++ photoapp/check.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['timeout'])) {
     $logintime = $_SESSION['timeout'];
     $differenceintime = time() - $logintime;
 
-    if ($differenceintime >= 350) { //session expiration
+    if ($differenceintime >= 50) { //session expiration
         session_unset();
         session_destroy();
         header("Location: index.php");
