5d614dd0c3abe12a6d1375e30c7a691afb451b3a
Lesson <1410733@rgu.ac.uk>
2016-04-30 22:27:51 +0100

    procedure

--- photoapp/photos.php
+++ photoapp/photos.php
@@ -14,7 +14,7 @@
 <body>
 
 <h4>Welcome <?php echo $login_user;?> <a href="photos.php" style="font-size:18px">Photos</a>||<a href="searchphotos.php" style="font-size:18px">Search</a>||<a href="logout.php" style="font-size:18px">Logout</a></h4>
-
+<h5>See your ip <?php $ip_add= $_SERVER ['REMOTE_ADDR']; echo $ip_add  ?></h5>
 <div id="photolist">
 	<?php echo $resultText;?>
 </div>
