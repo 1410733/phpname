512a0967b6374072ed639d6bea60e7bf8bdc0d9f
Lesson <1410733@rgu.ac.uk>
2016-05-01 18:44:58 +0100

    procedure

--- photoapp/search.php
+++ photoapp/search.php
@@ -1,16 +1,45 @@
 <?php
-
 $resultText = "";
+
+
+
+//Function to cleanup user input for xss
+function xss_cleaner($input_str) {
+    $return_str = str_replace( array('<','>',"'",'"',')','('), array('&lt;','&gt;','&apos;','&#x22;','&#x29;','&#x28;'), $input_str );
+    $return_str = str_ireplace( '%3Cscript', '', $return_str );
+    return $return_str;
+}
 if(isset($_POST["submit"]))
 {
-    $name = trim($_POST["username"]);
+    $name = $_POST["username"];
+    //filter user input
+    $name=mysqli_real_escape_string($db,$name);
+    $name=xss_cleaner($name);
 
-    $sql="SELECT userID FROM users WHERE username='$name'";
-    $result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_assoc($result);
-    if(mysqli_num_rows($result) == 1)
+    //instance of connection to dbase
+    $mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+    if ($mysqli->connect_errno) {
+        echo "Connection Failed, Check and Connect Again";// (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+
+    // Prepare OUT parameters
+    $mysqli->query("SET @userID=0");
+
+    if ( !$mysqli->query("CALL useridlookup('$name',@userID)"))  {
+        // echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+
+    //get user id into a variable
+
+    $res = $mysqli->query("SELECT @userID as userID");
+    $row = $res->fetch_assoc();
+    $userid=$row['userID'];//Get user ID
+
+
+    if ($userid >0)
     {
-        $searchID = trim($row['userID']);
+        $searchID = $row['userID'];
         $searchSql="SELECT title, photoID FROM photos WHERE userID='$searchID'";
         $searchresult=mysqli_query($db,$searchSql);
 
